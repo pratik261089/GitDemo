8e43b6a7f86da3cdcb54d723bb9a3729388d3638
pratik bhosle <pratikbhosle.21@gmail.com>
2025-11-11 01:33:46 -1200

    First Commit in git demo repo

---
+++ src/main/java/Automation/Selenium/App.java
@@ -0,0 +1,17 @@
+package Automation.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       WebDriverManager.chromedriver().setup();	
+       WebDriver driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.get("https://www.google.com");
+    }
+}
